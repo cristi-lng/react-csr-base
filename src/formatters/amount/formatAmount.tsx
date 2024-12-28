170bc9c47ebcb672a5d6927c0474f5080772eae0
cristi-lng <cristi.langa.85@gmail.com>
2024-12-29 01:30:36 +0200

    Update app dependencies to latest

--- src/formatters/amount/formatAmount.tsx
+++ src/formatters/amount/formatAmount.tsx
@@ -1,9 +1,11 @@
+import { useShallow } from 'zustand/shallow';
+
 import { FormatAmountArgs } from 'src/formatters/amount/formatAmountArgs';
 import { useSessionStore } from 'src/stores/sessionStore/sessionStore';
 import { amountFormatter } from 'src/formatters/amount/amountFormatter';
 
 function FormatAmount({ amount }: Pick<FormatAmountArgs, 'amount'>) {
-  const [locale, currency] = useSessionStore((state) => [state.locale, state.currency]);
+  const [locale, currency] = useSessionStore(useShallow((state) => [state.locale, state.currency]));
   return amountFormatter.format({ amount, currency, locale });
 }
 
